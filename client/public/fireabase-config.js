8dee612e65d1cd59cc2d7fd60b4c628162b47c3e
Kikuchi Ken <ken@nuasa.org>
2023-08-08 19:23:08 +0900

    Firebaseの設定を修正

--- client/public/fireabase-config.js
+++ client/public/fireabase-config.js
@@ -1,6 +1,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAW2tIcjuW0sLquzreACDqlk81ZNyuG_RM",
-  authDomain: "windview-311109.firebaseapp.com",
+  //authDomain: "windview-311109.firebaseapp.com",
+  authDomain: "windview-311109.web.app",
   projectId: "windview-311109",
   storageBucket: "windview-311109.appspot.com",
   messagingSenderId: "525761458401",
