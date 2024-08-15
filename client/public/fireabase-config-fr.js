28b72c966a18ffa884e8d32edacd12e470285d7a
Kikuchi Ken <ken@nuasa.org>
2024-08-15 19:56:20 +0900

    改行コードの更新

---
+++ client/public/fireabase-config-fr.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCfggjiVCCNECD4tWpl72RzYLaaR9ftOZw",
+  authDomain: "windview-fr.web.app",
+  projectId: "windview-fr",
+  storageBucket: "windview-fr.appspot.com",
+  messagingSenderId: "492947892736",
+  appId: "1:492947892736:web:d00ffa60818325ea5b06e5",
+  measurementId: "G-N6KY79G2JT"
+};
